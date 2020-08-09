9aa3d97f91bc1d9f30aede59c7c2c305f2f32d65
Dana Lu <dlu200774@gmail.com>
2020-08-09 09:32:24 -0400

    method stubs created

--- kargers_alg.py
+++ kargers_alg.py
@@ -2,11 +2,11 @@
 
 import numpy as np
 
-# Represent graph as an adj matrix where G[u][v] = # of edges from node U to node V
-# For simplicity, each of the computers is labelled 0, 1, 2, ... N. 
-
-# This class represents a graph data structure with data intialized from a file
+'''
+This class represents a graph data structure with data intialized from a file
+'''
 class Graph:
+    # Represent graph as an adj matrix where G[u][v] = no. of undirected edges from node U to node V
     ''' 
     :param filename: the filename formatted where 
                         the first line contains N, the number of nodes in the graph
@@ -19,18 +19,18 @@ class Graph:
         data_file = open(filename, 'r')
 
         # the number of nodes in this graph
-        n = data_file.readline()
+        n = int(data_file.readline())
 
         # initialize an NxN adjacency matrix for our graph
-        self.graph = np.zeros((int(n), int(n)))
+        self.graph = np.zeros((n, n))
 
         # read in the edges from the file
         for line in data_file:
-            nodes = line.split() # splits the string on whitespace
+            nodes = list(map(int, line.split())) # splits the string on whitespace
             
-            self.graph[int(nodes[0])][int(nodes[1])] += 1
+            self.graph[nodes[0]][nodes[1]] += 1
             # we have an undirected graph, so A->B implies B->A
-            self.graph[int(nodes[1])][int(nodes[0])] += 1
+            self.graph[nodes[1]][nodes[0]] += 1
 
         data_file.close()
 
@@ -50,15 +50,32 @@ contracts the edge between nodeA and nodeB in graph
 def contract(graph, nodeA, nodeB):
     pass   
 
-''' main function for Karger's Min-Cut application '''
-if __name__ == '__main__':
-    graph = Graph(filename= 'data.txt')
-    graph.printGraph()
+'''
+:param graph: the graph to randomly pick an edge from
+:param W: an array where W[i] is the sum of the values of the 
+            i-th row in the upper-right triangle of the adj matrix.
+randomly and uniformly selects an edge from graph
+'''
+def random_pick(graph, W):
+    pass
 
-    # keep going while there are 2 vertices in the graph
+'''
+:param graph: the graph to find the min-cut of
+utilizes karger's algorithm to find the min-cut of graph
+Note: this is a Monte Carlo algorithm, meaning the returned cut is
+not necessarily the min.
+'''
+def kargers(graph):
+    # keep going while there are more than 2 vertices in the graph
     while():
         pass
 
     # min-cut is represented by the remaining 2 supernodes
     print()
+
+''' main function for Karger's Min-Cut application '''
+if __name__ == '__main__':
+    # For convinience, each of the N computers in the data file is labelled 0, 1, 2, ... N. 
+    graph = Graph(filename= 'data.txt')
+    graph.printGraph()
 
